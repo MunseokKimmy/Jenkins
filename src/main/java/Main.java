f392b0c33ff3bd63d790b2f4b24eb78220f8630a
Munseok Kim <munseok.kim98@gmail.com>
2021-02-26 16:43:36 -0700

    Bruh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main() throws IOException {
+    public static void main(String[] args) throws IOException {
         System.out.println("Welcome to the calculator.");
         Calculator calculator = new Calculator();
         // Using Scanner for Getting Input from User
@@ -14,31 +14,32 @@ public class Main {
             input = in.nextLine();
             if (input.contains("add")){
                 String[] inputs = input.split(" ");
-                System.out.println(calculator.add(Integer.getInteger(inputs[1]), Integer.getInteger(inputs[2])));
+                int answer = calculator.add(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+                System.out.println(answer);
             }
             else if(input.contains("subtract")){
                 String[] inputs = input.split(" ");
-                System.out.println(calculator.subtract(Integer.getInteger(inputs[1]), Integer.getInteger(inputs[2])));
+                System.out.println(calculator.subtract(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2])));
 
             }
             else if(input.contains("multiply")){
                 String[] inputs = input.split(" ");
-                System.out.println(calculator.multiply(Integer.getInteger(inputs[1]), Integer.getInteger(inputs[2])));
+                System.out.println(calculator.multiply(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2])));
 
             }
             else if (input.contains("divide")){
                 String[] inputs = input.split(" ");
-                System.out.println(calculator.divide(Integer.getInteger(inputs[1]), Integer.getInteger(inputs[2])));
+                System.out.println(calculator.divide(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2])));
 
             }
             else if(input.contains("fibonacci")){
                 String[] inputs = input.split(" ");
-                System.out.println(calculator.fibonacciNumberFinder(Integer.getInteger(inputs[1])));
+                System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(inputs[1])));
 
             }
             else if(input.contains("binary")){
                 String[] inputs = input.split(" ");
-                System.out.println(calculator.intToBinaryNumber(Integer.getInteger(inputs[1])));
+                System.out.println(calculator.intToBinaryNumber(Integer.parseInt(inputs[1])));
             }
         }
 
